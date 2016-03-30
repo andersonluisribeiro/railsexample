873bc690968cd08dfea1e1b38114a81b37af8a70
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2016-03-30 18:51:56 -0300

    cadastro de categorias

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330205439) do
+ActiveRecord::Schema.define(version: 20160330215135) do
+
+  create_table "categoria", force: :cascade do |t|
+    t.string   "descricao",  limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "clientes", force: :cascade do |t|
     t.string   "nome",       limit: 255
